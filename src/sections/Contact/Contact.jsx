2da6292d681555dff528750fc16fccfac2ccace0
ProjectCisco <sga.accounts@proton.me>
2025-02-23 03:40:19 +0000

    Sections Update: Contact and Skills < Not Started

---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+import styles from './ContactStyles.module.css';
+
+function Contact() {
+  return (
+    <section id="contact" className={styles.container}>Contact</section>
+  )
+}
+
+export default Contact
---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,9 @@
+import styles from './SkillsStyles.module.css'
+
+function Skills() {
+  return (
+    <section id="skills" className={styles.container}>Skills</section>
+  )
+}
+
+export default Skills
