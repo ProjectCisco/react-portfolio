960415aa7606d698cf784662fa4ccd89c5f543d1
ProjectCisco <sga.accounts@proton.me>
2025-03-05 20:12:47 +0000

    Refactor ProjectsCard component: Update styling to use CSS modules and improve layout

--- src/sections/Projects/ProjectsCard.jsx
+++ src/sections/Projects/ProjectsCard.jsx
@@ -1,27 +1,26 @@
 import PropTypes from "prop-types";
 import { SocialIcon, FolderIcon, TechIcon } from "../../components/Icons";
-import styles from "./ProjectsStyles.module.css";
-
+import iconStyles from "../../components/iconsStyles.module.css";       
+import projectStyles from "./ProjectsStyles.module.css";
 function ProjectsCard({ title, description, href, github, tech1, tech2, tech3, tech4 }) {
   const techs = [tech1, tech2, tech3, tech4].filter(Boolean);
 
   return (
-    <div className={styles.projectCard}>
-      <div className={styles.projectHeader}>
-        <div className={styles.folderTitle}>
+    <div className={projectStyles.projectCard}>
+      <div className={projectStyles.projectHeader}>
+        <div className={projectStyles.folderTitle}>
           <FolderIcon />
-          <h3 className={styles.projectTitle}>{title}</h3>
+          <h3 className={projectStyles.projectTitle}>{title}</h3>
         </div>
-        <div className={styles.projectIcons}>
+        <div className={iconStyles.projectIcons}>
           {href && <SocialIcon kind="external" href={href} />}
           {github && <SocialIcon kind="github" href={github} />}
         </div>
       </div>
-      <p className={styles.projectDescription}>{description}</p>
-
-      <div className={styles.techStack}>
+      <p className={projectStyles.projectDescription}>{description}</p>
+      <div className={projectStyles.techStack}>
         {techs.map((tech, index) => (
-          <div key={index} className={styles.techItem}>
+          <div key={index} className={projectStyles.techItem}>
             <TechIcon kind={tech} size={18} /> <span>{tech.toUpperCase()}</span>
           </div>
         ))}
